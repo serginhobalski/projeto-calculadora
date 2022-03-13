bbcc09a1eb59ad8f4f56af19d645b892ea5cdd17
Sergio Obalski Filho <serginhobalski@gmail.com>
2022-03-13 16:53:17 -0300

    Initial Scripts

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,29 @@
+class CalcController {
+
+    constructor(){
+
+        this._displayCalc = "0";
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(value){
+        this._displayCalc = value;
+    }
+
+    get currentDate(){
+        return this._currentDate;
+    }
+
+    set currentDate(value){
+        this._currentDate = value;
+    }
+}
