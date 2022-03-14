2535f30ad72f11c346f72c36d0fb38269f7020d9
Sergio Obalski Filho <serginhobalski@gmail.com>
2022-03-14 08:49:56 -0300

    update

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -9,6 +9,13 @@ class CalcController {
 
     initialize(){
 
+        let displayCalcEl = document.querySelector("#display");
+        let dateCalcEl = document.querySelector("#data");
+        let timeCalcEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4567";
+        dateCalcEl.innerHTML = "21/03/2022";
+        timeCalcEl.innerHTML = "07:30";
     }
 
     get displayCalc(){
